9707960d61013dd0916e46b5bbff5b07e592637d
Malek Hakim <hakimelek@gmail.com>
2024-04-28 16:20:40 -0400

    Add names tag

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,9 +1,10 @@
 const TAGS = [
   { slug: "nes-bkri", name: "Nes Bekri Kalou" },
+  { slug: "nsfw", name: "Mouch 9oddem Bouk (+18)" },
   { slug: "animals", name: "7ayawenet" },
+  { slug: "names", name: "Names (+18)" },
   { slug: "ghacha", name: "Ghacha (+18)" },
   { slug: "sayings", name: "Akwel machhoura" },
-  { slug: "nsfw", name: "Mouch 9oddem Bouk (+18)" },
 ];
 
 export { TAGS };
